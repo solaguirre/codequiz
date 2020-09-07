4b0d82fd248fe6df284df4d3f372c8aaed140dec
solaguirre <68126593+solaguirre@users.noreply.github.com>
2020-09-07 14:32:51 -0700

    Added countdown timer and declared constants needed for quiz.

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
+
+// Constants
+const quiz = document.querySelector("#quiz");
+const questions = document.querySelector("#questions")
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+const timeEl = document.querySelector(".time");
+const buttons = document.querySelector("#buttons");
+const logInitials = document.querySelector("#logInitials");
+
+// Click start button and jumbotron is hidden and replaced with quiz questions...
+
+// Click on a button and quiz starts, timer starts and I am presented with a question
+
 // Generating Quiz fx
+function buildQuiz() {
 
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
-
-    function showQuestions(questions, quizContainer) {
-        // insert code here
-    }
-
-    function showResults(questions, quizContainer, resultsContainer) {
-        // insert code here  
-    }
-
-    // show the questions
-    showQuestions(questions, quizContainer);
-
-    // when user clicks submit, show results
-    submitButton.onclick = function () {
-        showResults(questions, quizContainer, resultsContainer);
-    }
 }
 
+function showResults
 // Timer 
 
-let secondsLeft = 60;
+let secondsLeft = 90;
 
-function setTime() {
-
+function setTime(end) {
 
     let timerInterval = setInterval(function () {
         secondsLeft--;
@@ -34,129 +34,157 @@ function setTime() {
             clearInterval(timerInterval);
         }
     }, 1000);
+    // add another if or if else statement to subtract time?
 
-}
+    // if (question)
+};
 
 setTime();
+console.log(secondsLeft);
 
 
-// QUESTIONS
+// quizQUESTIONS
 
 let quizQuestions = [
 
     {
         question: "What country is JBalvin from?",
         answers: {
-            button1: 'Colombia',
-            button2: 'Panama',
-            button3: 'Mexico',
-            button4: 'France'
+            buttona: 'Colombia',
+            buttonb: 'Panama',
+            buttonc: 'Mexico',
+            buttond: 'France'
         },
-        correctAnswer: 'button1'
+        correctAnswer: 'buttona'
     },
     {
         question: "Which OG Reggaetonero is referred to as 'El Abayarde'?",
         answers: {
-            button1: 'Bad Bunny',
-            button2: 'Don Omar',
-            button3: 'Daddy Yankee',
-            button4: 'Tego Calderon'
+            buttona: 'Bad Bunny',
+            buttonb: 'Don Omar',
+            buttonc: 'Daddy Yankee',
+            buttond: 'Tego Calderon'
         },
-        correctAnswer: 'button4'
+        correctAnswer: 'buttond'
     },
 
     {
         question: "Which Reggaetonero is featured on Aventura's 'Ella y Yo'?",
         answers: {
-            button1: 'Tito "El Bambino"',
-            button2: 'Don Omar',
-            button3: 'Alexis',
-            button4: 'Nicky Jam'
+            buttona: 'Tito "El Bambino"',
+            buttonb: 'Don Omar',
+            buttonc: 'Alexis',
+            buttond: 'Nicky Jam'
         },
-        correctAnswer: 'button2'
+        correctAnswer: 'buttonb'
     },
     {
         question: "Which Reggaeton artist sings on 'Quiero Bailar'?",
         answers: {
-            button1: 'Maluma',
-            button2: 'Becky G',
-            button3: 'Ivy Queen',
-            button4: 'Arcangel'
+            buttona: 'Maluma',
+            buttonb: 'Becky G',
+            buttonc: 'Ivy Queen',
+            buttond: 'Arcangel'
         },
-        correctAnswer: 'button3'
+        correctAnswer: 'buttonc'
     },
     {
         question: "In what year was Daddy Yankees hit song 'Gasolina' released?",
         answers: {
-            button1: '2006',
-            button2: '2003',
-            button3: '1992',
-            button4: '2010'
+            buttona: '2006',
+            buttonb: '2003',
+            buttonc: '1992',
+            buttond: '2010'
         },
-        correctAnswer: 'button4'
+        correctAnswer: 'buttond'
     }
 ];
+
+const renderQuizBtn = document.querySelector("#beginQuiz");
+const buttona = document.querySelector("a");
+const buttonb = document.querySelector("b");
+const buttonc = document.querySelector("c");
+const buttond = document.querySelector("d");
 
 let index = 0;
 let currentQuestion = quizQuestions[0];
 
-nextQuestion();
+let renderQuizEl = document.querySelector("#quizQuestions");
+
+function renderQuizQuestion() {
+    renderQuizEl.innerHTML =
 
 
-// function to wrap around, to reuse lines of code
-
-function nextQuestion() {
-    currentQuestion = quizQuestions[index]
-    question.textContent = currentQuestion.question;
-    button1.textContent = currentQuestion.answers[0];
-    button2.textContent = currentQuestion.answers[1];
-    button3.textContent = currentQuestion.answers[2];
-    button4.textContent = currentQuestion.answers[3];
+        nextQuestion();
 
 
+    // function to wrap around, to reuse lines of code
+
+    function nextQuestion() {
+        currentQuestion = quizQuestions[index]
+        question.textContent = currentQuestion.question;
+        button1.textContent = currentQuestion.answers[0];
+        button2.textContent = currentQuestion.answers[1];
+        button3.textContent = currentQuestion.answers[2];
+        button4.textContent = currentQuestion.answers[3];
+    }
+
+    // Event listeners for clicking answers 
+    button1.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (button1.textContent === currentQuestion.correctAnswer) {
+            alert("Correct");
+        } else {
+            alert("wrong");
+        }
+        index++;
+        nextQuestion();
+    });
+
+    button2.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (button2.textContent === currentQuestion.correctAnswer) {
+            alert("Correct");
+        } else {
+            alert("wrong");
+        }
+        index++;
+        nextQuestion();
+    });
+
+    button3.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (button3.textContent === currentQuestion.correctAnswer) {
+            alert("Correct");
+        } else {
+            alert("wrong");
+        }
+        index++;
+        nextQuestion();
+    });
+
+    button4.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (button4.textContent === currentQuestion.correctAnswer) {
+            alert("Correct");
+        } else {
+            alert("wrong");
+        }
+        index++;
+        nextQuestion();
+    });
 }
+// WHEN I answer a question incorrectly
 
-// Event listeners for clicking answers 
-button1.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (button1.textContent === currentQuestion.correctAnswer) {
-        alert("Correct");
-    } else {
-        alert("wrong");
-    }
-    index++;
-    nextQuestion();
-});
 
-button2.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (button2.textContent === currentQuestion.correctAnswer) {
-        alert("Correct");
-    } else {
-        alert("wrong");
-    }
-    index++;
-    nextQuestion();
-});
+// THEN time is subtracted from the clock
 
-button3.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (button3.textContent === currentQuestion.correctAnswer) {
-        alert("Correct");
-    } else {
-        alert("wrong");
-    }
-    index++;
-    nextQuestion();
-});
 
-button4.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (button4.textContent === currentQuestion.correctAnswer) {
-        alert("Correct");
-    } else {
-        alert("wrong");
-    }
-    index++;
-    nextQuestion();
-});
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score // 
+
+
+
+
